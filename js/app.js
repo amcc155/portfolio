43f942d23420e05d28c00e938b177d5831f4b7c0
aidmccor <aidmccor@iu.edu>
2024-01-02 16:09:11 -0600

    mobile test

--- js/app.js
+++ js/app.js
@@ -2,18 +2,31 @@ const menu = document.querySelector('#menu')
 const dropdown = document.querySelector('.dropdown')
 let isDropDownActive
 
-menu.addEventListener('click',(e) =>{
-e.target.classList.add('swirl-in-fwd')
+menu.addEventListener('click', (e) => {
+    e.target.classList.add('swirl-in-fwd')
 
-dropdown.style.display = 'block'
-isDropDownActive = true
+    dropdown.style.display = 'block'
+    isDropDownActive = true
 })
 
-if(isDropDownActive){
+if (isDropDownActive) {
     console.log('active')
-    document.addEventListener('click',()=>{
+    document.addEventListener('click', () => {
         dropdown.style.display = 'none'
         isDropDownActive = false;
-    
+
     })
+}
+
+
+//https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp 
+var prevScrollpos = window.scrollY || window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.scrollY || window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector('.social-container').style.top = "0";
+    } else {
+        document.querySelector('.social-container').style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
 }
