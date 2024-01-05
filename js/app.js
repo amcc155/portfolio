07d9b625b56961cfcb3a4ec39e1d8659aca86f60
aidmccor <aidmccor@iu.edu>
2024-01-04 18:53:02 -0600

    test again

--- js/app.js
+++ js/app.js
@@ -1,32 +1,39 @@
-const menu = document.querySelector('#menu')
-const dropdown = document.querySelector('.dropdown')
-let isDropDownActive
+const menu = document.querySelector('#menu');
+const dropdown = document.querySelector('.dropdown');
+
+let isDropDownActive = false;
 
 menu.addEventListener('click', (e) => {
-    e.target.classList.add('swirl-in-fwd')
+  e.stopPropagation();
+  e.target.classList.add('swirl-in-fwd');
+  dropdown.style.opacity = '1';
+  console.log('ok')
+  isDropDownActive = true;
 
-    dropdown.style.display = 'block'
-    isDropDownActive = true
-})
+  // Add event listener to close dropdown when clicking outside
+  document.addEventListener('click', closeDropdown);
+});
 
-if (isDropDownActive) {
-    console.log('active')
-    document.addEventListener('click', () => {
-        dropdown.style.display = 'none'
-        isDropDownActive = false;
+function closeDropdown(e) {
+  if (!dropdown.contains(e.target)) {
+    dropdown.style.opacity = '0';
+    isDropDownActive = false;
 
-    })
+    // Remove the event listener after closing the dropdown
+    document.removeEventListener('click', closeDropdown);
+  }
 }
+
 
 
 //https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp 
 var prevScrollpos = window.scrollY || window.pageYOffset;
 window.onscroll = function () {
-    var currentScrollPos = window.scrollY || window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector('.social-container').style.top = "0";
-    } else {
-        document.querySelector('.social-container').style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
+  var currentScrollPos = window.scrollY || window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('.social-container').style.top = "0";
+  } else {
+    document.querySelector('.social-container').style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
